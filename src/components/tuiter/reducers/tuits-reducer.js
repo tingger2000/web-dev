501939298ee978ec3030ac29d6d8f2ac7f89ef26
tingger2000 <yuting.ger@gmail.com>
2022-04-02 16:31:46 -0400

    a8 REST sp22

--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,39 +1,22 @@
-import tuits from "../data/tuits.json";
+import {
+  CREATE_TUIT,
+  DELETE_TUIT,
+  FIND_ALL_TUITS,
+  UPDATE_TUIT,
+} from "../actions/tuits-action";
 
-const tuitsReducer = (state = tuits, action) => {
+const tuitsReducer = (state = [], action) => {
   switch (action.type) {
-    case "create-tuit":
-      const newTuit = {
-        tuit: action.tuit,
-        _id: new Date().getTime() + "",
-        postedBy: {
-          username: "ReactJS",
-        },
-        handle: "kayleeg",
-        avatarImage:
-          "https://t3.ftcdn.net/jpg/00/75/33/10/360_F_75331081_axIcnWZnT1RbSsPGlgLQaCftelG158KV.jpg",
-        stats: {
-          retuits: 111,
-          likes: 222,
-          replies: 333,
-        },
-      };
-      return [newTuit, ...state];
-    case "delete-tuit":
+    case CREATE_TUIT:
+      return [...state, action.newTuit];
+    case FIND_ALL_TUITS:
+      return action.tuits;
+    case DELETE_TUIT:
       return state.filter((tuit) => tuit._id !== action.tuit._id);
-    case "like-tuit":
-      return state.map((tuit) => {
-        if (tuit._id === action.tuit._id) {
-          if (tuit.liked === true) {
-            tuit.liked = false;
-            tuit.stats.likes--;
-          } else {
-            tuit.liked = true;
-            tuit.stats.likes++;
-          }
-        }
-        return tuit;
-      });
+    case UPDATE_TUIT:
+      return state.map((tuit) =>
+        tuit._id === action.tuit._id ? action.tuit : tuit
+      );
     default:
       return state;
   }
--- src/components/tuiter/tuit-list/tuit-list-item.js
+++ src/components/tuiter/tuit-list/tuit-list-item.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { deleteTuit } from "../actions/tuits-action";
 import TuitStats from "./tuit-stats";
 import "../tuiter.css";
 
@@ -11,67 +12,54 @@ const TuitListItem = ({
     tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     attachments: {},
     avatarImage: "../../../images/react-blue.png",
-    stats: {},
+    likes: 0,
+    dislikes: 0,
   },
 }) => {
   const dispatch = useDispatch();
-  const deleteTuit = (tuit) => {
-    dispatch({ type: "delete-tuit", tuit });
-  };
 
   return (
     <li className="list-group-item px-4">
       <div className="row">
-        <div className="col-2 col-md-1 d-flex justify-content-center">
-          <img
-            className="rounded-circle"
-            src={tuit.avatarImage}
-            width="48px"
-            height="48px"
-            alt="..."
+        <div className="col-11 pe-0">
+          <div className="d-flex">
+            <div className="fw-bold">
+              {tuit.postedBy && tuit.postedBy.username}
+            </div>
+            <div className="text-secondary ps-1">
+              {tuit.handle && `@${tuit.handle}`}
+            </div>
+          </div>
+          <div>{tuit.tuit}</div>
+        </div>
+        <div className="col-1 p-0">
+          <i
+            onClick={() => deleteTuit(dispatch, tuit)}
+            className="btn py-0 fas fa-times"
           />
         </div>
-        <div className="col-10 col-md-11">
-          <div className="row">
-            <div className="col-11 pe-0">
-              <div className="d-flex">
-                <div className="fw-bold">
-                  {tuit.postedBy && tuit.postedBy.username}
-                </div>
-                <div className="text-secondary ps-1">@{tuit.handle}</div>
-              </div>
-              <div>{tuit.tuit}</div>
-            </div>
-            <div className="col-1 p-0">
-              <i
-                onClick={() => deleteTuit(tuit)}
-                className="btn py-0 fas fa-times"
-              />
-            </div>
-          </div>
-          <div>
-            {tuit.attachments && tuit.attachments.video && (
-              <iframe
-                className="wd-rounded-corners-20 w-100 py-2"
-                height="315"
-                src={`https://www.youtube.com/embed/${tuit.attachments.video}`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="fullscreen"
-              />
-            )}
-            {tuit.attachments && tuit.attachments.image && (
-              <img
-                className="wd-rounded-corners-20 w-100 py-2"
-                height="315px"
-                src={tuit.attachments.image}
-                alt="..."
-              />
-            )}
-          </div>
-          <TuitStats tuit={tuit} />
-        </div>
       </div>
+      <div>
+        {tuit.attachments && tuit.attachments.video && (
+          <iframe
+            className="wd-rounded-corners-20 w-100 py-2"
+            height="315"
+            src={`https://www.youtube.com/embed/${tuit.attachments.video}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="fullscreen"
+          />
+        )}
+        {tuit.attachments && tuit.attachments.image && (
+          <img
+            className="wd-rounded-corners-20 w-100 py-2"
+            height="315px"
+            src={tuit.attachments.image}
+            alt="..."
+          />
+        )}
+      </div>
+      <TuitStats tuit={tuit} />
     </li>
   );
 };
